b214215dfc65cd87fea3f57b969271dfb774e4bd
serhiilaptop <sergebrew@gmail.com>
2023-04-18 16:01:19 -0600

    contacts block

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -10,6 +10,9 @@ import website_icon from '../../assets/images/website_icon.svg';
 import facebook_icon from '../../assets/images/facebook_icon.svg';
 import instagram_icon from '../../assets/images/instagram_icon.svg';
 import twitter_icon from '../../assets/images/twitter_icon.svg';
+import email_icon from  '../../assets/images/email_icon.svg';
+import phone_icon from '../../assets/images/phone_icon.svg';
+
 import MainPageNav from "./MainPageNav";
 
 const MainPage = () => {
@@ -75,6 +78,23 @@ const MainPage = () => {
                 </div>
                 Web Developer with 2+ years' experience in React.js & Node.js. Lifelong learner, strong collaborator, mentor, and
                 problem-solver.
+              </div>
+              <div className={styles.contacts}>
+                <div className="row">
+                  <span className="text-center">Get in touch with me</span>
+                  <div className="col-12 col-md-6">
+                    <a className={styles.links} href="mailto:idaveee@gmail.com">
+                      <img width="40" src={email_icon} alt=""/>
+                      <p>idaveee@gmail.com</p>
+                    </a>
+                  </div>
+                  <div className="col-12 col-md-6">
+                    <a className={styles.links} href="tel:15875833791">
+                      <img width="40" src={phone_icon} alt=""/>
+                      <p>+1 (587) 583-3791</p>
+                    </a>
+                  </div>
+                </div>
               </div>
               <div className={styles.social}>
                 <ul>
